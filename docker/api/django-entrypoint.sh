530de696a048c61162a3411be86dda070d597b1c
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-01-21 22:14:37 -0600

    Create a Dockerfile
    Creates a Dockerfile that will be used for releases. This image uses the
    multistage build feature to considerably reduce the size of the final
    artifact.
    
    Drive-by:
    * Adjust the `django-entrypoint.sh` script
    * Use the release `Dockerfile` by default in the `Makefile`

--- docker/api/django-entrypoint.sh
+++ docker/api/django-entrypoint.sh
@@ -4,12 +4,8 @@ set -eo pipefail
 # Define variables.
 : ${RYR_API_API_PORT:=8000}
 
-# Debug.
-echo "PWD is ${PWD}"
-ls -lh
-
 # Migrate db, so we have the latest db schema.
-python manage.py migrate
+django-admin migrate
 
 # Start development server on public ip interface, on port 8000.
 exec gunicorn ${RYR_API_API_OPTS} -b 0.0.0.0:${RYR_API_API_PORT} api.wsgi
