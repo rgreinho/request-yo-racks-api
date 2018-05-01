9d1ef95140db0415b6941d5945eebd33243000bc
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-04-30 19:13:37 -0500

    Add /health endpoint
    Adds a `/health` endpoint to check the health of the api.

--- api/apps/api/urls.py
+++ api/apps/api/urls.py
@@ -5,6 +5,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api.apps.api import views
 
 urlpatterns = [
+    url(r'^health$', views.Health.as_view()),
     url(r'^places/(?P<latlong>[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?))$',
         views.PlaceList.as_view()),
     url(r'^place/(?P<pid>[0-9a-zA-Z\-]+)/$', views.PlaceDetails.as_view()),
