7f81d95c6c7bd7ce3b8c0339bc86128028683ccc
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-05-06 18:12:39 -0500

    Fix api URLs
    The API URLs got somehow messed up. This patch fixes them.

--- api/apps/api/urls.py
+++ api/apps/api/urls.py
@@ -5,10 +5,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api.apps.api import views
 
 urlpatterns = [
-    url(r'^health$', views.Health.as_view()),
-    url(r'^places/(?P<latlong>[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?))$',
+    url(r'health$', views.Health.as_view()),
+    url(r'$', views.Health.as_view()),
+    url(r'places/(?P<latlong>[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?))$',
         views.PlaceList.as_view()),
-    url(r'^place/(?P<pid>[0-9a-zA-Z\-]+)/$', views.PlaceDetails.as_view()),
+    url(r'place/(?P<pid>[0-9a-zA-Z\-]+)/$', views.PlaceDetails.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
