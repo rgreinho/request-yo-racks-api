ad74fe0169a703ffb44fc2bcd2a7905a136853ce
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-05-08 19:27:16 -0500

    Fix routes (#34)
    The routes were incorrectly defined, always redirecting to `/health`.

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 from django.conf.urls import include
 from django.contrib import admin
 
+from api.apps.api import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.Health.as_view()),
     url(r'^', include('api.apps.api.urls')),
 ]
