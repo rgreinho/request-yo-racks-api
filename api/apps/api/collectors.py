481625e4a71a1b45f9e218f60db3b20c00e3ea00
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-06-22 14:36:00 -0500

    Implement a cleaner collector interface (#50)
    Uses a dataclass to specify which information will be returned by the
    API in order to generate the email.
    
    Fixes request-yo-racks/api#22

--- api/apps/api/collectors.py
+++ api/apps/api/collectors.py
@@ -3,8 +3,31 @@
 import abc
 import urllib.parse
 
+from dataclasses import dataclass
+
 import googlemaps  # pylint: disable=import-error
 import requests
+
+
+@dataclass
+class BusinessInfo:
+    """Define the information identifying a business."""
+
+    name: str = ''
+    address: str = ''
+    latitude: float = 0.0
+    longitude: float = 0.0
+    type: str = ''
+    phone: str = ''
+    email: str = ''
+    contact_name: str = ''
+    website: str = ''
+    parking_info: str = ''
+    extra_info: str = ''
+
+    def geolocation(self) -> str:
+        """Compute the business geolocation."""
+        return f'{self.latitude},{self.longitude}'
 
 
 class AbstractCollector:
@@ -14,25 +37,7 @@ class AbstractCollector:
 
     def __init__(self):
         """Initialize the collector."""
-        # Business properties.
-        self.name = ''
-        self.address = ''
-        self.phone = ''
-
-    @property
-    def business_name(self):
-        """Return the business name."""
-        return self.name or ''
-
-    @property
-    def business_address(self):
-        """Return the business address."""
-        return self.address or ''
-
-    @property
-    def business_phone_number(self):
-        """Return the phone number."""
-        return self.phone or ''
+        self.result = None
 
     @abc.abstractmethod
     def search_place(self, address, terms=None):
@@ -52,9 +57,19 @@ class AbstractCollector:
         """
         Retrieve the details of a place.
 
-        This function populates the `name`, `address`, and `phone` properties of this instance.
+        This function returns the raw result of the search and caches it in the `self.result` property.
 
         :param str place_id: the ID of a place
+        :rtype: dict
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def to_business_info(self):
+        """
+        Convert the raw data to a BusinessInfo object.
+
+        :rtype: BusinessInfo object
         """
         raise NotImplementedError
 
@@ -153,7 +168,7 @@ class YelpCollector(AbstractRestCollector):
         """
         Retrieve the details of a place.
 
-        This function populates the `name`, `address`, and `phone` properties of this instance.
+        This function returns the raw result of the search and caches it in the `self.result` property.
 
         :param str place_id: the ID of a place
         :returns: A dictionnary containing the detailed information of the place matching the `place_id`.
@@ -165,14 +180,30 @@ class YelpCollector(AbstractRestCollector):
 
         # Query the server.
         response = requests.get(url, headers=self.headers)
-        response_json = response.json()
+        self.result = response.json()
 
         # Populate the properties.
-        self.name = response_json.get('name', '')
-        self.address = ' '.join(response_json.get('location', {}).get('display_address', ''))
-        self.phone = response_json.get('display_phone', '')
 
-        return response_json
+        return self.result
+
+    def to_business_info(self):
+        """Convert the raw data to a BusinessInfo object."""
+        # Ensure we have data to convert.
+        if not self.result:
+            return None
+        if not self.result.get('result'):
+            return None
+
+        # Define convenience variables.
+        r = self.result.get('result')
+        location = r.get('location', {})
+
+        # Populate the business information.
+        b = BusinessInfo()
+        b.name = r.get('name', '')
+        b.address = ' '.join(location.get('display_address', ''))
+        b.phone = r.get('display_phone', '')
+        return b
 
 
 class GoogleCollector(AbstractClientCollector):
@@ -186,8 +217,7 @@ class GoogleCollector(AbstractClientCollector):
         self.places_client = None
 
     def authenticate(self, api_key):
-        """Authenticate against Yelp."""
-        # Search Business.
+        """Authenticate against Google."""
         self.places_client = googlemaps.Client(key=api_key)
 
     def search_place(self, address, terms=None):
@@ -213,14 +243,30 @@ class GoogleCollector(AbstractClientCollector):
         :returns: A dictionnary containing the detailed information of the place matching the `place_id`.
         :rtype: dict
         """
-        result = self.places_client.place(place_id)
+        self.result = self.places_client.place(place_id)
+        return self.result
 
-        # Populate the properties.
-        self.name = result.get('result', {}).get('name', '')
-        self.address = result.get('result', {}).get('formatted_address', '')
-        self.phone = result.get('result', {}).get('phone', '')
+    def to_business_info(self):
+        """Convert the raw data to a BusinessInfo object."""
+        # Ensure we have data to convert.
+        if not self.result:
+            return None
+        if not self.result.get('result'):
+            return None
 
-        return result
+        # Define convenience variables.
+        r = self.result.get('result')
+        location = r.get('geometry', {}).get('location', {})
+
+        # Populate the business information.
+        b = BusinessInfo()
+        b.name = r.get('name', '')
+        b.address = r.get('formatted_address', '')
+        b.phone = r.get('formatted_phone_number', '')
+        b.website = r.get('website', '')
+        b.latitude = location.get('lat', 0.0)
+        b.longitude = location.get('lng', 0.0)
+        return b
 
 
 class CollectorClient:
@@ -266,7 +312,7 @@ class CollectorClient:
             self.collector = GoogleCollector()
             self.collector.authenticate(self.api_key)
         else:
-            raise ValueError('The "{}" provider is not supported.'.format(self.provider))
+            raise ValueError(f'The "{self.provider}" provider is not supported.')
 
     def search_place(self, address, terms=None):
         """
@@ -284,23 +330,13 @@ class CollectorClient:
         """
         Retrieve the details of a place.
 
-        This function retrievesd the `name`, `address`, and `phone` properties of the place matching the
+        This function retrieves the `name`, `address`, and `phone` properties of the place matching the
         place_id.
 
         :param str place_id: the ID of a place
         :returns: A dictionnary containing the detailed information of the place matching the `place_id`.
         :rtype: dict
         """
-        details = self.collector.retrieve_place_details(place_id)
-        return details
-
-    def retrieve_place_details(self, address, terms=None):
-        """Retrieve the details of a place.
-
-        :param str address: business address
-        :param str terms: Optional. Search term (e.g. "food", "restaurants"). If term isn’t included we
-            search everything. The term keyword also accepts business names such as "Starbucks".
-
-        """
-        place_id = self.search_place(address, terms=terms)
-        return self.retrieve_details(place_id)
+        _ = self.collector.retrieve_place_details(place_id)
+        b = self.collector.to_business_info()
+        return self.collector.to_business_info().__dict__ if b else {}
