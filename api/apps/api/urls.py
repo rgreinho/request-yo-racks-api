883add9f12d9de7abdb20f37acd4c92d8adfc92a
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-09-29 14:02:45 -0500

    Fix place endpoint
    Removes the trailing slash.

--- api/apps/api/urls.py
+++ api/apps/api/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'health$', views.Health.as_view()),
     url(r'places/(?P<latlong>[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?))$',
         views.PlaceList.as_view()),
-    url(r'place/$', views.PlaceDetails.as_view()),
+    url(r'place$', views.PlaceDetails.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
