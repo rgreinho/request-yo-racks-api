d9dbedc2f75ad8854da06c4317f305c6229868c0
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-12-28 16:24:15 -0600

    Fix the redoc URL
    The ReDoc URL has to be computed based on the environment the API is run
    on. This patch achieves it by adding a `BASE_URL`settings which will
    contain the appropriate value.

--- api/settings/local.py
+++ api/settings/local.py
@@ -4,5 +4,10 @@ import os
 from api.settings.common import *  # noqa
 
 DEBUG = True
-environment_based_spec_dir = '/usr/local/etc/ryr-api' if os.environ.get('KUBERNETES_PORT') else 'openapi'
+if os.environ.get('KUBERNETES_PORT'):
+    environment_based_spec_dir = '/usr/local/etc/ryr-api'
+    BASE_URL = 'http://api.192.168.99.100.nip.io'
+else:
+    environment_based_spec_dir = 'openapi'
+    BASE_URL = f'http://0.0.0.0:{PORT}'
 SPECIFICATION_DIR = os.path.join(BASE_DIR, environment_based_spec_dir)
